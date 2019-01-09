13014b15379bd549667a5939e8a7c2d7aec4dbec
BrianDX <boris.zaitsev@devexpress.com>
2019-01-09 13:19:59 +0300

    example content

---
+++ CS/WindowCalculationExample/Form1.cs
@@ -0,0 +1,106 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWin;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowCalculationExample
+{
+    public partial class Form1 : Form
+    {
+        private string timedTabContainerName;
+        private Timer tabTimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            dashboardViewer1.ConfigureDataConnection += DashboardViewer1_ConfigureDataConnection;
+            dashboardViewer1.DashboardItemControlCreated += DashboardViewer1_DashboardItemControlCreated;
+            dashboardViewer1.DashboardItemControlUpdated += DashboardViewer1_DashboardItemControlUpdated;
+            dashboardViewer1.DashboardItemClick += DashboardViewer1_DashboardItemClick;
+
+            TabPageLoadHelper helper = new TabPageLoadHelper("CalculationProductSalesSource.xml");
+            helper.PrepareDashboard();
+            dashboardViewer1.Dashboard = helper.Dashboard;
+            
+
+            ApplyScatterChartWindowCalculations(helper.ScatterChartItemName);
+            ApplyPivotWindowCalculations(helper.PivotItemName);
+
+            timedTabContainerName = helper.TabContainerName;
+
+            StartTimer();
+        }
+
+        private void ApplyPivotWindowCalculations(string pivotItemName)
+        {
+            PivotDashboardItem pivotItem = dashboardViewer1.Dashboard.Items[pivotItemName] as PivotDashboardItem;
+            if (pivotItem != null)
+            {
+                Measure extendedPrice = new Measure("Extended Price")
+                {
+                    Name = "Diff",
+                    ShowGrandTotals = false
+                };
+                PivotWindowDefinition pivotWindowDefinition = new PivotWindowDefinition();
+                pivotWindowDefinition.DefinitionMode = PivotWindowDefinitionMode.Columns;
+                extendedPrice.WindowDefinition = pivotWindowDefinition;
+                extendedPrice.Calculation = new DifferenceCalculation() { DifferenceType = DifferenceType.Absolute };
+                pivotItem.Values.Add(extendedPrice);
+            }
+        }
+
+        private void ApplyScatterChartWindowCalculations(string scatterChartName)
+        {
+            ScatterChartDashboardItem scatterChart = dashboardViewer1.Dashboard.Items[scatterChartName] as ScatterChartDashboardItem;
+            if (scatterChart != null)
+            {
+                scatterChart.AxisXMeasure.Calculation = new DifferenceCalculation() { DifferenceType = DifferenceType.Percentage };
+                scatterChart.AxisXMeasure.WindowDefinition = new ScatterWindowDefinition();
+                scatterChart.AxisYMeasure.Expression = "WindowMedian(ToDouble(Sum([Extended Price]) - Lookup(Sum([Extended Price]), -1)) / Lookup(Sum([Extended Price]), -1), 0, 1)";
+                scatterChart.AxisYMeasure.WindowDefinition = new ScatterWindowDefinition();
+            }
+        }
+
+        private void DashboardViewer1_ConfigureDataConnection(object sender, DashboardConfigureDataConnectionEventArgs e)
+        {
+            ExtractDataSourceConnectionParameters parameters = e.ConnectionParameters as ExtractDataSourceConnectionParameters;
+            if (parameters != null)
+                parameters.FileName = Path.GetFileName(parameters.FileName);
+        }
+        private void DashboardViewer1_DashboardItemControlUpdated(object sender, DashboardItemControlEventArgs e)
+        {
+            if (e.PivotGridControl != null)
+                e.PivotGridControl.BestFit();
+        }
+
+        private void DashboardViewer1_DashboardItemControlCreated(object sender, DashboardItemControlEventArgs e)
+        {
+            if (e.PivotGridControl != null)
+                e.PivotGridControl.BestFit();
+        }
+
+
+        private void StartTimer()
+        {
+            tabTimer.Interval = 2000;
+            tabTimer.Tick += TabTimer_Tick;
+            tabTimer.Start();
+        }
+
+        private void TabTimer_Tick(object sender, EventArgs e)
+        {
+            if (timedTabContainerName != null)
+            {
+                int selectedIndex = dashboardViewer1.GetSelectedTabPageIndex(timedTabContainerName);
+                int pageCount = ((TabContainerDashboardItem)dashboardViewer1.Dashboard.Items[timedTabContainerName]).TabPages.Count;
+                dashboardViewer1.SetSelectedTabPage(timedTabContainerName, ++selectedIndex % pageCount);
+            }
+        }
+
+        private void DashboardViewer1_DashboardItemClick(object sender, DevExpress.DashboardWin.DashboardItemMouseActionEventArgs e)
+        {
+            tabTimer.Enabled = !tabTimer.Enabled;
+        }
+
+    }
+}
---
+++ CS/WindowCalculationExample/TabPageLoadHelper.cs
@@ -0,0 +1,124 @@
+﻿using DevExpress.DashboardCommon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WindowCalculationExample
+{
+    class TabPageLoadHelper
+    {
+        private Dashboard originalDashboard = new Dashboard();
+        private TabContainerDashboardItem destinationParentContainer;
+        public Dashboard Dashboard { get; set; }
+        public string TabContainerName { get; private set; }
+        public string ScatterChartItemName { get; private set; }
+        public string PivotItemName { get; private set; }
+
+        public TabPageLoadHelper(string xmldashboardFile)
+        {
+            originalDashboard.LoadFromXml(Path.Combine(Environment.CurrentDirectory, xmldashboardFile));
+        }
+
+        public void PrepareDashboard()
+        {
+            Dashboard targetDashboard = new Dashboard();
+            destinationParentContainer = new TabContainerDashboardItem();
+            targetDashboard.Items.Add(destinationParentContainer);
+            TabContainerName = destinationParentContainer.ComponentName;
+
+            DashboardTabPage tabPage = CopyDashboardToTabPage(targetDashboard, originalDashboard);
+
+            this.Dashboard = targetDashboard;
+            DashboardTabPage tabPage1 = destinationParentContainer.CreateTabPage();
+            targetDashboard.RebuildLayout();
+
+            CopyTabPage(tabPage, tabPage1);
+
+            ScatterChartItemName = targetDashboard.Items.Single(item => (item is ScatterChartDashboardItem) && (item.ParentContainer == tabPage1)).ComponentName;
+            PivotItemName = targetDashboard.Items.Single(item => item is PivotDashboardItem && (item.ParentContainer == tabPage1)).ComponentName;
+
+        }
+
+        private DashboardTabPage CopyDashboardToTabPage(Dashboard targetDashboard, Dashboard originalDashboard)
+        {
+            DashboardTabPage tabPage = destinationParentContainer.CreateTabPage();
+            DashboardLayoutTabPage layoutPage = new DashboardLayoutTabPage(tabPage);
+            DashboardLayoutTabContainer layoutTabContainer = new DashboardLayoutTabContainer(destinationParentContainer);
+            layoutTabContainer.ChildNodes.Add(layoutPage);
+
+            IEnumerable<DashboardItem> itemsAndGroups = originalDashboard.Items.Union(originalDashboard.Groups).Where(item => !(item is TabContainerDashboardItem));
+            foreach (DashboardItem item in itemsAndGroups)
+            {
+                if (item.ParentContainer == null)
+                {
+                    if (!(item is TabContainerDashboardItem))
+                    {
+                        targetDashboard.Items.Add(item);
+                        item.ParentContainer = tabPage;
+                    }
+                }
+            }
+
+            DashboardLayoutGroup layoutRoot = originalDashboard.LayoutRoot;
+            layoutPage.ChildNodes.Add(layoutRoot);
+            targetDashboard.LayoutRoot = new DashboardLayoutGroup();
+            targetDashboard.LayoutRoot.ChildNodes.Add(layoutTabContainer);
+            return tabPage;
+        }
+
+        public void CopyTabPage(DashboardTabPage tabPageSource, DashboardTabPage tabPageDestination)
+        {
+            Dashboard targetDashboard = this.Dashboard;
+            DashboardLayoutTabPage tabPageSourceLayout = tabPageSource.Dashboard.LayoutRoot.FindRecursive(tabPageSource);
+            DashboardLayoutTabPage tabPageDestinationLayout = tabPageSource.Dashboard.LayoutRoot.FindRecursive(tabPageDestination);
+            LayoutCopy(tabPageSourceLayout, tabPageDestinationLayout);
+        }
+
+        void LayoutCopy(DashboardLayoutNode copyFrom, DashboardLayoutNode copyTo)
+        {
+            copyTo.Weight = copyFrom.Weight;
+            if ((copyTo is DashboardLayoutGroup) && (copyFrom is DashboardLayoutGroup))
+                ((DashboardLayoutGroup)copyTo).Orientation = ((DashboardLayoutGroup)copyFrom).Orientation;
+            if (copyFrom is DashboardLayoutGroup)
+            {
+                foreach (DashboardLayoutNode currentNode in ((DashboardLayoutGroup)copyFrom).ChildNodes)
+                {
+                    DashboardLayoutNode node = new DashboardLayoutItem();
+                    DashboardItem item = currentNode.DashboardItem;
+                    if (item != null) {
+                        node.DashboardItem = CopyItemFromPageToPage(item, destinationParentContainer.TabPages[1]);
+                        node.Weight = currentNode.Weight;
+                    }
+                    if (currentNode is DashboardLayoutGroup)
+                    {
+                        node = CopyTree(currentNode);
+                    }
+                    ((DashboardLayoutGroup)copyTo).ChildNodes.Add(node);
+                }
+            }
+        }
+
+        private DashboardItem CopyItemFromPageToPage(DashboardItem item, IDashboardItemContainer itemContainer)
+        {
+            DashboardItem itemCopy = item.CreateCopy();
+            itemCopy.ComponentName = GetNameOnNextPage(item.ComponentName);
+            itemCopy.Name = GetNameOnNextPage(item.Name);
+            itemCopy.ParentContainer = itemContainer;
+            Dashboard.Items.Add(itemCopy);
+            return itemCopy;
+        }
+
+        private string GetNameOnNextPage(string name)
+        {
+            return name + "_1";
+        }
+
+        private DashboardLayoutNode CopyTree(DashboardLayoutNode currentNode)
+        {
+            DashboardLayoutGroup dashboardLayoutNode = new DashboardLayoutGroup();
+            LayoutCopy(currentNode, dashboardLayoutNode);
+            return dashboardLayoutNode;
+        }
+    }
+}
