ef211cafe77a60332e11fe093eaa8e59debae94c
DevExpressExampleBot <fakeEmail>
2019-01-09 14:15:44 +0300

    Source auto update [skip ci]

---
+++ VB/WindowCalculationExample/Form1.vb
@@ -0,0 +1,97 @@
+﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardWin
+Imports System
+Imports System.IO
+Imports System.Windows.Forms
+
+Namespace WindowCalculationExample
+    Partial Public Class Form1
+        Inherits Form
+
+        Private timedTabContainerName As String
+        Private tabTimer As New Timer()
+        Public Sub New()
+            InitializeComponent()
+            AddHandler dashboardViewer1.ConfigureDataConnection, AddressOf DashboardViewer1_ConfigureDataConnection
+            AddHandler dashboardViewer1.DashboardItemControlCreated, AddressOf DashboardViewer1_DashboardItemControlCreated
+            AddHandler dashboardViewer1.DashboardItemControlUpdated, AddressOf DashboardViewer1_DashboardItemControlUpdated
+            AddHandler dashboardViewer1.DashboardItemClick, AddressOf DashboardViewer1_DashboardItemClick
+
+            Dim helper As New TabPageLoadHelper("CalculationProductSalesSource.xml")
+            helper.PrepareDashboard()
+            dashboardViewer1.Dashboard = helper.Dashboard
+
+
+            ApplyScatterChartWindowCalculations(helper.ScatterChartItemName)
+            ApplyPivotWindowCalculations(helper.PivotItemName)
+
+            timedTabContainerName = helper.TabContainerName
+
+            StartTimer()
+        End Sub
+
+        Private Sub ApplyPivotWindowCalculations(ByVal pivotItemName As String)
+            Dim pivotItem As PivotDashboardItem = TryCast(dashboardViewer1.Dashboard.Items(pivotItemName), PivotDashboardItem)
+            If pivotItem IsNot Nothing Then
+                Dim extendedPrice As New Measure("Extended Price") With { _
+                    .Name = "Diff", _
+                    .ShowGrandTotals = False _
+                }
+                Dim pivotWindowDefinition As New PivotWindowDefinition()
+                pivotWindowDefinition.DefinitionMode = PivotWindowDefinitionMode.Columns
+                extendedPrice.WindowDefinition = pivotWindowDefinition
+                extendedPrice.Calculation = New DifferenceCalculation() With {.DifferenceType = DifferenceType.Absolute}
+                pivotItem.Values.Add(extendedPrice)
+            End If
+        End Sub
+
+        Private Sub ApplyScatterChartWindowCalculations(ByVal scatterChartName As String)
+            Dim scatterChart As ScatterChartDashboardItem = TryCast(dashboardViewer1.Dashboard.Items(scatterChartName), ScatterChartDashboardItem)
+            If scatterChart IsNot Nothing Then
+                scatterChart.AxisXMeasure.Calculation = New DifferenceCalculation() With {.DifferenceType = DifferenceType.Percentage}
+                scatterChart.AxisXMeasure.WindowDefinition = New ScatterWindowDefinition()
+                scatterChart.AxisYMeasure.Expression = "WindowMedian(ToDouble(Sum([Extended Price]) - Lookup(Sum([Extended Price]), -1)) / Lookup(Sum([Extended Price]), -1), 0, 1)"
+                scatterChart.AxisYMeasure.WindowDefinition = New ScatterWindowDefinition()
+            End If
+        End Sub
+
+        Private Sub DashboardViewer1_ConfigureDataConnection(ByVal sender As Object, ByVal e As DashboardConfigureDataConnectionEventArgs)
+            Dim parameters As ExtractDataSourceConnectionParameters = TryCast(e.ConnectionParameters, ExtractDataSourceConnectionParameters)
+            If parameters IsNot Nothing Then
+                parameters.FileName = Path.GetFileName(parameters.FileName)
+            End If
+        End Sub
+        Private Sub DashboardViewer1_DashboardItemControlUpdated(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs)
+            If e.PivotGridControl IsNot Nothing Then
+                e.PivotGridControl.BestFit()
+            End If
+        End Sub
+
+        Private Sub DashboardViewer1_DashboardItemControlCreated(ByVal sender As Object, ByVal e As DashboardItemControlEventArgs)
+            If e.PivotGridControl IsNot Nothing Then
+                e.PivotGridControl.BestFit()
+            End If
+        End Sub
+
+
+        Private Sub StartTimer()
+            tabTimer.Interval = 2000
+            AddHandler tabTimer.Tick, AddressOf TabTimer_Tick
+            tabTimer.Start()
+        End Sub
+
+        Private Sub TabTimer_Tick(ByVal sender As Object, ByVal e As EventArgs)
+            If timedTabContainerName IsNot Nothing Then
+                Dim selectedIndex As Integer = dashboardViewer1.GetSelectedTabPageIndex(timedTabContainerName)
+                Dim pageCount As Integer = CType(dashboardViewer1.Dashboard.Items(timedTabContainerName), TabContainerDashboardItem).TabPages.Count
+                selectedIndex += 1
+                dashboardViewer1.SetSelectedTabPage(timedTabContainerName, selectedIndex Mod pageCount)
+            End If
+        End Sub
+
+        Private Sub DashboardViewer1_DashboardItemClick(ByVal sender As Object, ByVal e As DevExpress.DashboardWin.DashboardItemMouseActionEventArgs)
+            tabTimer.Enabled = Not tabTimer.Enabled
+        End Sub
+
+    End Class
+End Namespace
---
+++ VB/WindowCalculationExample/TabPageLoadHelper.vb
@@ -0,0 +1,133 @@
+﻿Imports DevExpress.DashboardCommon
+Imports System
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Linq
+
+Namespace WindowCalculationExample
+    Friend Class TabPageLoadHelper
+        Private originalDashboard As New Dashboard()
+        Private destinationParentContainer As TabContainerDashboardItem
+        Public Property Dashboard() As Dashboard
+        Private privateTabContainerName As String
+        Public Property TabContainerName() As String
+            Get
+                Return privateTabContainerName
+            End Get
+            Private Set(ByVal value As String)
+                privateTabContainerName = value
+            End Set
+        End Property
+        Private privateScatterChartItemName As String
+        Public Property ScatterChartItemName() As String
+            Get
+                Return privateScatterChartItemName
+            End Get
+            Private Set(ByVal value As String)
+                privateScatterChartItemName = value
+            End Set
+        End Property
+        Private privatePivotItemName As String
+        Public Property PivotItemName() As String
+            Get
+                Return privatePivotItemName
+            End Get
+            Private Set(ByVal value As String)
+                privatePivotItemName = value
+            End Set
+        End Property
+
+        Public Sub New(ByVal xmldashboardFile As String)
+            originalDashboard.LoadFromXml(Path.Combine(Environment.CurrentDirectory, xmldashboardFile))
+        End Sub
+
+        Public Sub PrepareDashboard()
+            Dim targetDashboard As New Dashboard()
+            destinationParentContainer = New TabContainerDashboardItem()
+            targetDashboard.Items.Add(destinationParentContainer)
+            TabContainerName = destinationParentContainer.ComponentName
+
+            Dim tabPage As DashboardTabPage = CopyDashboardToTabPage(targetDashboard, originalDashboard)
+
+            Me.Dashboard = targetDashboard
+            Dim tabPage1 As DashboardTabPage = destinationParentContainer.CreateTabPage()
+            targetDashboard.RebuildLayout()
+
+            CopyTabPage(tabPage, tabPage1)
+
+            ScatterChartItemName = targetDashboard.Items.Single(Function(item) (TypeOf item Is ScatterChartDashboardItem) AndAlso (item.ParentContainer Is tabPage1)).ComponentName
+            PivotItemName = targetDashboard.Items.Single(Function(item) TypeOf item Is PivotDashboardItem AndAlso (item.ParentContainer Is tabPage1)).ComponentName
+
+        End Sub
+
+        Private Function CopyDashboardToTabPage(ByVal targetDashboard As Dashboard, ByVal originalDashboard As Dashboard) As DashboardTabPage
+            Dim tabPage As DashboardTabPage = destinationParentContainer.CreateTabPage()
+            Dim layoutPage As New DashboardLayoutTabPage(tabPage)
+            Dim layoutTabContainer As New DashboardLayoutTabContainer(destinationParentContainer)
+            layoutTabContainer.ChildNodes.Add(layoutPage)
+
+            Dim itemsAndGroups As IEnumerable(Of DashboardItem) = originalDashboard.Items.Union(originalDashboard.Groups).Where(Function(item) Not (TypeOf item Is TabContainerDashboardItem))
+            For Each item As DashboardItem In itemsAndGroups
+                If item.ParentContainer Is Nothing Then
+                    If Not (TypeOf item Is TabContainerDashboardItem) Then
+                        targetDashboard.Items.Add(item)
+                        item.ParentContainer = tabPage
+                    End If
+                End If
+            Next item
+
+            Dim layoutRoot As DashboardLayoutGroup = originalDashboard.LayoutRoot
+            layoutPage.ChildNodes.Add(layoutRoot)
+            targetDashboard.LayoutRoot = New DashboardLayoutGroup()
+            targetDashboard.LayoutRoot.ChildNodes.Add(layoutTabContainer)
+            Return tabPage
+        End Function
+
+        Public Sub CopyTabPage(ByVal tabPageSource As DashboardTabPage, ByVal tabPageDestination As DashboardTabPage)
+            Dim targetDashboard As Dashboard = Me.Dashboard
+            Dim tabPageSourceLayout As DashboardLayoutTabPage = tabPageSource.Dashboard.LayoutRoot.FindRecursive(tabPageSource)
+            Dim tabPageDestinationLayout As DashboardLayoutTabPage = tabPageSource.Dashboard.LayoutRoot.FindRecursive(tabPageDestination)
+            LayoutCopy(tabPageSourceLayout, tabPageDestinationLayout)
+        End Sub
+
+        Private Sub LayoutCopy(ByVal copyFrom As DashboardLayoutNode, ByVal copyTo As DashboardLayoutNode)
+            copyTo.Weight = copyFrom.Weight
+            If (TypeOf copyTo Is DashboardLayoutGroup) AndAlso (TypeOf copyFrom Is DashboardLayoutGroup) Then
+                CType(copyTo, DashboardLayoutGroup).Orientation = CType(copyFrom, DashboardLayoutGroup).Orientation
+            End If
+            If TypeOf copyFrom Is DashboardLayoutGroup Then
+                For Each currentNode As DashboardLayoutNode In CType(copyFrom, DashboardLayoutGroup).ChildNodes
+                    Dim node As DashboardLayoutNode = New DashboardLayoutItem()
+                    Dim item As DashboardItem = currentNode.DashboardItem
+                    If item IsNot Nothing Then
+                        node.DashboardItem = CopyItemFromPageToPage(item, destinationParentContainer.TabPages(1))
+                        node.Weight = currentNode.Weight
+                    End If
+                    If TypeOf currentNode Is DashboardLayoutGroup Then
+                        node = CopyTree(currentNode)
+                    End If
+                    CType(copyTo, DashboardLayoutGroup).ChildNodes.Add(node)
+                Next currentNode
+            End If
+        End Sub
+
+        Private Function CopyItemFromPageToPage(ByVal item As DashboardItem, ByVal itemContainer As IDashboardItemContainer) As DashboardItem
+            Dim itemCopy As DashboardItem = item.CreateCopy()
+            itemCopy.ComponentName = GetNameOnNextPage(item.ComponentName)
+            itemCopy.Name = GetNameOnNextPage(item.Name)
+            itemCopy.ParentContainer = itemContainer
+            Dashboard.Items.Add(itemCopy)
+            Return itemCopy
+        End Function
+
+        Private Function GetNameOnNextPage(ByVal name As String) As String
+            Return name & "_1"
+        End Function
+
+        Private Function CopyTree(ByVal currentNode As DashboardLayoutNode) As DashboardLayoutNode
+            Dim dashboardLayoutNode As New DashboardLayoutGroup()
+            LayoutCopy(currentNode, dashboardLayoutNode)
+            Return dashboardLayoutNode
+        End Function
+    End Class
+End Namespace
